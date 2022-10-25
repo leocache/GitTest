098d1071e7fe0906eb178a4907cbfaf35d873c25
“lihao” <2223128221@qq.com>
2022-10-25 14:39:10 +0800

    second commit

--- src/main/java/com/gitTest/GitTest.java
+++ src/main/java/com/gitTest/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello the world");
         System.out.println("Hello the world2");
+        System.out.println("Hello the world3");
+
     }
 }
