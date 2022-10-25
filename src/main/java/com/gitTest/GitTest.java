e59e6b2a5f11a740701991008bf65f8fc850563b
“lihao” <2223128221@qq.com>
2022-10-25 11:47:29 +0800

    first commit

---
+++ src/main/java/com/gitTest/GitTest.java
@@ -0,0 +1,12 @@
+package com.gitTest;
+
+/**
+ * @author ：Leo
+ * @date ：Created in 2022/10/25 11:43
+ * @description：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello the world");
+    }
+}
